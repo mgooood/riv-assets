0ec8c3225a10d9577107b2fe0f5dfe6dc029c7b0
mgood <mark.gooood@gmail.com>
2015-12-11 08:36:04 -0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,16 @@ var iconfontCss = require('gulp-iconfont-css');
 var runTimestamp = Math.round(Date.now()/1000);
 
 var fontName = 'icons';
+var cssClass = '';
 
 gulp.task('iconfont', function(){
   gulp.src(['assets/svgs/*.svg'])
     .pipe(iconfontCss({
-      fontName: fontName
+      fontName: fontName,
+      cssClass: cssClass
     }))
     .pipe(iconfont({
-      fontName: 'icons', // required
+      fontName: fontName, // required
       appendUnicode: true, // recommended option
       formats: ['ttf', 'eot', 'woff', 'svg'], // default, 'woff2' and 'svg' are available
       timestamp: runTimestamp, // recommended to get consistent builds when watching files
