8d8b9ef8d5d68038498754799442a7b3ea150082
mgood <mark.gooood@gmail.com>
2015-12-11 13:06:31 -0500

    rearranging and updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,34 @@
-var gulp = require('gulp');
-
+var gulp = require("gulp");
+var rename = require("gulp-rename");
 var iconfont = require('gulp-iconfont');
-var iconfontCss = require('gulp-iconfont-css');
-var runTimestamp = Math.round(Date.now()/1000);
+var consolidate = require('gulp-consolidate');
 
-var fontName = 'icons';
-var cssClass = '';
+var fontName = 'icons'; // set name of your symbol font
+var template = 'fontawesome-style'; // you can also choose 'foundation-style'
 
-gulp.task('iconfont', function(){
-  gulp.src(['assets/svgs/*.svg'])
-    .pipe(iconfontCss({
-      fontName: fontName,
-      cssClass: cssClass
-    }))
-    .pipe(iconfont({
-      fontName: fontName, // required
-      appendUnicode: true, // recommended option
-      formats: ['ttf', 'eot', 'woff', 'svg'], // default, 'woff2' and 'svg' are available
-      timestamp: runTimestamp, // recommended to get consistent builds when watching files
-     }))
-    .pipe(gulp.dest('assets/fonts/'));
+gulp.task('svgtoicons', function(){
+  gulp.src('assets/svgs/*.svg') // you can also choose 'symbol-font-16px.sketch'
+    .pipe(iconfont({ fontName: fontName, normalize: true }))
+    .on('glyphs', function(glyphs) {
+      var options = {
+        glyphs: glyphs.map(function(glyph) {
+          // this line is needed because gulp-iconfont has changed the api from 2.0
+          return { name: glyph.name, codepoint: glyph.unicode[0].charCodeAt(0) }
+        }),
+        fontName: fontName,
+        fontPath: '', // set path to font (from your CSS file if relative)
+        className: '' // set class name in your CSS
+      };
+      gulp.src('assets/templates/' + template + '.css')
+        .pipe(consolidate('lodash', options))
+        .pipe(rename({ basename:fontName }))
+        .pipe(gulp.dest('dist/css/')); // set path to export your CSS
+
+      // if you don't need sample.html, remove next 4 lines
+      gulp.src('assets/templates/' + template + '.html')
+        .pipe(consolidate('lodash', options))
+        .pipe(rename({ basename:'sample' }))
+        .pipe(gulp.dest('dist/')); // set path to export your sample HTML
+    })
+    .pipe(gulp.dest('dist/fonts/')); // set path to export your fonts
 });
